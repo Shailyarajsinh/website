5982cc9bb067fad66f65578b059456a5459ce5cb
Shailyarajsinh Mahida <mahidashailyaraj@gmail.com>
2025-01-08 13:52:55 +0530

    Update route path in main.jsx to support an array of paths for Homepage

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import NotFound from './components/not found/NotFound'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path={'/home' && '/'} element={<Homepage />} />
+      <Route path={['/home', '/']} element={<Homepage />} />
       <Route path='*' element={<NotFound/>} />
     </Route>
   )
